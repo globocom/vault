ffb310f901a1faf09a426b8b49d1202cf52a609f
hendrix <hendrix@corp.globo.com>
2015-08-06 14:50:21 -0300

    Update Models

--- actionlogger/models.py
+++ actionlogger/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 class Audit(models.Model):
 	id = models.AutoField(primary_key=True)
-	user = models.CharField(max_length=30, null=False)
-	action = models.CharField(max_length=30, null=False)
-	item = models.CharField(max_length=30, null=False)
+	user = models.CharField(max_length=30, null=False, blank=False)
+	action = models.CharField(max_length=30, null=False, blank=False)
+	item = models.CharField(max_length=30, null=False, blank=False)
 	through = models.CharField(max_length=30,default='vault')
 	created_at = models.DateField(auto_now=True)
 
