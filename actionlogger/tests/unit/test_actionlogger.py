048fb7eee45c5ab5dd1d4eca4cc8dfc41fd84b53
hendrix <hendrix@corp.globo.com>
2015-08-18 15:33:57 -0300

    fix pep8

--- actionlogger/tests/unit/test_actionlogger.py
+++ actionlogger/tests/unit/test_actionlogger.py
@@ -7,23 +7,24 @@ from actionlogger import ActionLogger, ActionNotFound
 
 import datetime
 
-audit = Audit(user='TestUser', action='create', item='A Test Project')   
+audit = Audit(user='TestUser', action='create', item='A Test Project')
+
 
 class TestActionLogger(TestCase):
 
-	def setUp(self):
-		self.actionlog = ActionLogger()
+    def setUp(self):
+        self.actionlog = ActionLogger()
 
-	@patch("actionlogger.syslog.syslog")
-	def test_logging_an_action(self, syslog_mock):
-		self.actionlog.log('TestUser', 'create', 'A Test Project')
-		syslog_mock.assert_called_with(6, "User (TestUser) CREATED: A Test Project")
+    @patch("actionlogger.syslog.syslog")
+    def test_logging_an_action(self, syslog_mock):
+        self.actionlog.log('TestUser', 'create', 'A Test Project')
+        syslog_mock.assert_called_with(6, "User (TestUser) CREATED: A Test Project")
 
-	def test_log_with_an_invalid_action_raises_actionnotfound(self):
-		self.assertRaises(ActionNotFound, self.actionlog.log,
+    def test_log_with_an_invalid_action_raises_actionnotfound(self):
+        self.assertRaises(ActionNotFound, self.actionlog.log,
                           'TestUser', 'drop', 'A Test Project')
 
-	def test_audit_action(self):
-		self.actionlog.savedb(audit)
-		line = Audit.objects.get(id=audit.id)        
-		self.assertEqual(line.user,'TestUser')
+    def test_audit_action(self):
+        self.actionlog.savedb(audit)
+        line = Audit.objects.get(id=audit.id)
+        self.assertEqual(line.user, 'TestUser')
