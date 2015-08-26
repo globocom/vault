ec8f1615defb91548b81bae37ed1895d72312674
caio.passos <caio.passos@corp.globo.com>
2015-08-26 19:13:19 -0300

    Inicio de criacao da tela de informacao do project criado

--- identity/views.py
+++ identity/views.py
@@ -3,7 +3,7 @@
 import logging
 
 from django.contrib import messages
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic.base import View, TemplateView
@@ -273,6 +273,23 @@ class ListProjectView(BaseProjectView):
 
         return context
 
+
+class CreateProjectSuccessView(LoginRequiredMixin, TemplateView):
+    template_name = 'identity/project_create_success.html'
+
+    def get(self, request, *args, **kwargs):
+
+        context = self.get_context_data(request=request, **kwargs)
+
+        return self.render_to_response(context)
+
+    def get_context_data(self, **kwargs):
+        context = super(CreateProjectSuccessView, self).get_context_data(**kwargs)
+
+        request = kwargs.get('request')
+        context['project_info'] = request.session.get('project_info')
+
+        return context
 
 class CreateProjectView(BaseProjectView):
     template_name = "identity/project_create.html"
