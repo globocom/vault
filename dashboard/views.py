7405677e1ed9a8b566a01668355985bc252c02cb
Alan <alan@corp.globo.com>
2015-08-27 18:17:07 -0300

    DashboardView agora so precisa do template

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,11 +7,3 @@ from vault.views import LoginRequiredMixin
 
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = "dashboard/dashboard.html"
-
-    def __init__(self, *args, **kwargs):
-        self.keystone = None
-        return super(DashboardView, self).__init__(*args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        context = super(DashboardView, self).get_context_data(**kwargs)
-        return context
