89a47b1e3f8f48d5a08c1bb7f2a572cf7bbca48a
Alan <alan@corp.globo.com>
2015-08-27 17:04:18 -0300

    verificando is_superuser e is_staff para mostrar ou não o batao do admin

--- identity/views.py
+++ identity/views.py
@@ -362,7 +362,7 @@ class DeleteProjectView(BaseProjectView):
     def post(self, request, *args, **kwargs):
         form = DeleteProjectConfirm(initial={'user': request.user}, data=request.POST)
         self.keystone = Keystone(request)
-       return HttpResponseRedirect('http://www.globo.com')
+        return HttpResponseRedirect('http://www.globo.com')
 
 
 
