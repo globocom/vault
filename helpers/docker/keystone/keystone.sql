7b87dd97a61851808af60e109108f6fa702ee274
Alan <avanselmo@gmail.com>
2019-12-23 15:37:09 -0300

    Docker Keystone

---
+++ helpers/docker/keystone/keystone.sql
@@ -0,0 +1,5 @@
+create database if not exists keystone;
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' \
+IDENTIFIED BY 'KEYSTONE_DBPASS';
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' \
+IDENTIFIED BY 'KEYSTONE_DBPASS';
