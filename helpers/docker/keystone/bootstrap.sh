cf7505f3bb87ce293fb78f00f24057960481e76c
Alan <avanselmo@gmail.com>
2019-12-26 03:27:43 -0300

    Keystone docker working with Pike version

--- helpers/docker/keystone/bootstrap.sh
+++ helpers/docker/keystone/bootstrap.sh
@@ -1,78 +1,119 @@
 #!/bin/bash
+exec > >(tee -i /keystone-v3.log)
+exec 2>&1
+
 set -x
 
-# Init the arguments
-ADMIN_TOKEN=${ADMIN_TOKEN:-294a4c8a8a475f9b9836}
-ADMIN_TENANT_NAME=${ADMIN_TENANT_NAME:-admin}
-ADMIN_USER_NAME=${ADMIN_USERNAME:-admin}
-ADMIN_PASSWORD=${ADMIN_PASSWORD:-ADMIN_PASS}
-ADMIN_EMAIL=${ADMIN_EMAIL:-${ADMIN_USER_NAME}@example.com}
+# Openstack Keystone
 
-OS_TOKEN=$ADMIN_TOKEN
-OS_URL=${OS_AUTH_URL:-"http://${HOSTNAME}:35357/v3"}
-OS_IDENTITY_API_VERSION=3
+: ${IPADDR:=127.0.0.1}
 
-CONFIG_FILE=/etc/keystone/keystone.conf
-SQL_SCRIPT=${SQL_SCRIPT:-/root/keystone.sql}
+# Identity service configuration
+: ${OS_IDENTITY_API_VERSION:=3}
+: ${OS_IDENTITY_SERVICE_REGION:=RegionOne}
+: ${OS_IDENTITY_SERVICE_NAME:=keystone}
+: ${OS_IDENTITY_ADMIN_DOMAIN:=default}
+: ${OS_IDENTITY_ADMIN_PROJECT:=admin}
+: ${OS_IDENTITY_ADMIN_USERNAME:=admin}
+: ${OS_IDENTITY_ADMIN_PASSWD:=ADMIN_PASS}
+: ${OS_IDENTITY_ADMIN_ROLE:=admin}
+: ${OS_IDENTITY_URL_ADMIN:=http://${IPADDR}:35357}
+: ${OS_IDENTITY_URL_INTERNAL:=http://${IPADDR}:5000}
+: ${OS_IDENTITY_URL_PUBLIC:=http://${IPADDR}:5000}
 
-# if env | grep -qi MYSQL_ROOT_PASSWORD && test -e $SQL_SCRIPT; then
-#     MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-$MYSQL_ENV_MYSQL_ROOT_PASSWORD}
-#     MYSQL_HOST=${MYSQL_HOST:-mysql}
-#     sed -i "s#^connection.*=.*#connection = mysql://keystone:KEYSTONE_DBPASS@${MYSQL_HOST}/keystone#" $CONFIG_FILE
-#     mysql -uroot -p$MYSQL_ROOT_PASSWORD -h $MYSQL_HOST <$SQL_SCRIPT
-# fi
+# Object store configuration
+: ${OS_OBJECTSTORE_SERVICE_REGION:=RegionOne}
+: ${OS_OBJECTSTORE_SERVICE_NAME:=swift}
+: ${OS_OBJECTSTORE_SERVICE_DESC:=Swift Object Storage}
+: ${OS_OBJECTSTORE_DOMAIN:=default}
+: ${OS_OBJECTSTORE_PROJECT:=swift}
+: ${OS_OBJECTSTORE_USERNAME:=swift}
+: ${OS_OBJECTSTORE_PASSWD:=SWIFT_PASS}
+: ${OS_OBJECTSTORE_ROLE:=admin}
+: ${OS_OBJECTSTORE_URL_ADMIN:=http://vault_swift/v1}
+: ${OS_OBJECTSTORE_URL_INTERNAL:=http://vault_swift/v1/AUTH_%(tenant_id)s}
+: ${OS_OBJECTSTORE_URL_PUBLIC:=http://vault_swift/v1/AUTH_%(tenant_id)s}
 
-rm -f $SQL_SCRIPT
+echo '> Configuring Keystone ...'
 
-# update keystone.conf
-sed -i "s#^admin_token.*=.*#admin_token = $ADMIN_TOKEN#" $CONFIG_FILE
+# Set log to stderr for Docker
+# openstack-config --set /etc/keystone/keystone.conf DEFAULT use_stderr True
 
-# Populate the Identity service database
+# Use a local sqlite database for demo purposes
+# openstack-config --set /etc/keystone/keystone.conf database connection 'sqlite:////var/lib/keystone/keystone.db'
+
+keystone-manage credential_setup \
+  --keystone-user root \
+  --keystone-group root
+
+keystone-manage fernet_setup \
+  --keystone-user root \
+  --keystone-group root
+
 keystone-manage db_sync
 
-# Initialize Fernet keys
-keystone-manage fernet_setup --keystone-user root --keystone-group root
-mv /etc/keystone/default_catalog.templates /etc/keystone/default_catalog
+keystone-manage bootstrap \
+  --bootstrap-project-name "$OS_IDENTITY_ADMIN_PROJECT" \
+  --bootstrap-username "$OS_IDENTITY_ADMIN_USERNAME" \
+  --bootstrap-username "$OS_IDENTITY_ADMIN_USERNAME" \
+  --bootstrap-password "$OS_IDENTITY_ADMIN_PASSWD" \
+  --bootstrap-role-name "$OS_IDENTITY_ADMIN_ROLE" \
+  --bootstrap-service-name "$OS_IDENTITY_SERVICE_NAME" \
+  --bootstrap-region-id "$OS_IDENTITY_SERVICE_REGION" \
+  --bootstrap-admin-url "$OS_IDENTITY_URL_ADMIN" \
+  --bootstrap-public-url "$OS_IDENTITY_URL_PUBLIC" \
+  --bootstrap-internal-url "$OS_IDENTITY_URL_INTERNAL"
 
-# start keystone service
+# Using uwsgi for demo purposes
+echo '> Starting Keystone admin service ...'
 uwsgi --http 0.0.0.0:35357 --wsgi-file $(which keystone-wsgi-admin) &
-# uwsgi --http 0.0.0.0:5000 --wsgi-file $(which keystone-wsgi-public) &
-sleep 5 # wait for start
+sleep 5
 
-export OS_TOKEN OS_URL OS_IDENTITY_API_VERSION
+echo '> Starting Keystone public service ...'
+uwsgi --http 0.0.0.0:5000 --wsgi-file $(which keystone-wsgi-admin) &
+sleep 5
 
-# Initialize account
-openstack service create --name keystone identity
-openstack endpoint create --region RegionOne identity public http://${HOSTNAME}:5000/v3
-openstack endpoint create --region RegionOne identity internal http://${HOSTNAME}:5000/v3
-openstack endpoint create --region RegionOne identity admin http://${HOSTNAME}:5000/v3
-openstack domain create --description "Default Domain" default
-openstack project create --domain default --description "Admin Project" admin
-openstack user create --domain default --password $ADMIN_PASSWORD admin
-openstack role create admin
-openstack role add --project admin --user admin admin
+while ! nc -z ${IPADDR} 35357; do
+  sleep 1
+done
 
-# Add Swift service
-# openstack service create --name swift object-storage
-# openstack endpoint create --region RegionOne object-storage public 'http://vault_swift/v1/AUTH_$(tenant_id)s'
-# openstack endpoint create --region RegionOne object-storage internal 'http://vault_swift/v1/AUTH_$(tenant_id)s'
-# openstack endpoint create --region RegionOne object-storage admin 'http://vault_swift/v1/AUTH_$(tenant_id)s'
-
-unset OS_TOKEN OS_URL
-
-# Write openrc to disk
-cat >~/openrc <<EOF
-export OS_PROJECT_DOMAIN_NAME=default
-export OS_USER_DOMAIN_NAME=default
-export OS_PROJECT_NAME=admin
-export OS_USERNAME=admin
-export OS_PASSWORD=${ADMIN_PASSWORD}
-export OS_AUTH_URL=http://${HOSTNAME}:35357/v3
-export OS_IDENTITY_API_VERSION=3
-export OS_IMAGE_API_VERSION=2
+# Admin credentials
+cat <<EOF >/keystone_adminrc
+export OS_IDENTITY_API_VERSION="$OS_IDENTITY_API_VERSION"
+export OS_AUTH_URL="$OS_IDENTITY_URL_ADMIN"
+export OS_USER_DOMAIN_ID="$OS_IDENTITY_ADMIN_DOMAIN"
+export OS_PROJECT_DOMAIN_ID="$OS_IDENTITY_ADMIN_DOMAIN"
+export OS_PROJECT_NAME="$OS_IDENTITY_ADMIN_PROJECT"
+export OS_USERNAME="$OS_IDENTITY_ADMIN_USERNAME"
+export OS_PASSWORD="$OS_IDENTITY_ADMIN_PASSWD"
 EOF
+source /keystone_adminrc
 
-cat ~/openrc
+# Create roles
+echo '> Creating Keystone roles ...'
+#openstack role create 'admin'
+#openstack role create '_member_'
+openstack role create 'swiftoperator'
+
+# Object store service
+echo '> Registering Object Store service ...'
+openstack project create "$OS_OBJECTSTORE_PROJECT"
+openstack user create --password "$OS_OBJECTSTORE_PASSWD" --project "$OS_OBJECTSTORE_PROJECT" "$OS_OBJECTSTORE_USERNAME"
+openstack role add --user "$OS_OBJECTSTORE_USERNAME" --project "$OS_OBJECTSTORE_PROJECT" "$OS_OBJECTSTORE_ROLE"
+openstack service create --name "$OS_OBJECTSTORE_SERVICE_NAME" --description "$OS_OBJECTSTORE_SERVICE_DESC" 'object-store'
+openstack endpoint create --region "$OS_OBJECTSTORE_SERVICE_REGION" 'object-store' public   "$OS_OBJECTSTORE_URL_PUBLIC"
+openstack endpoint create --region "$OS_OBJECTSTORE_SERVICE_REGION" 'object-store' internal "$OS_OBJECTSTORE_URL_INTERNAL"
+openstack endpoint create --region "$OS_OBJECTSTORE_SERVICE_REGION" 'object-store' admin    "$OS_OBJECTSTORE_URL_ADMIN"
+
+# Vault user
+openstack domain create "default"
+openstack project create "Vault"
+openstack user create --password "u_vault" --project "Vault" "u_vault"
+openstack role add --user "u_vault" --project "Vault" "admin"
+
+# echo '> Starting Keystone public service ...'
+# # /usr/bin/keystone-wsgi-public --port 5000
+# uwsgi --http 0.0.0.0:5000 --wsgi-file $(which keystone-wsgi-admin)
 
 # reboot services
 pkill uwsgi
