21e9052b2cf0cb6cb0a7498434ec4a2843008cf5
Julio Rama <julio.rama@corp.globo.com>
2020-01-09 20:00:24 -0300

    Create initial vault user and group during make docker-start

--- helpers/docker/vault/start.sh
+++ helpers/docker/vault/start.sh
@@ -6,6 +6,7 @@ python helpers/docker/vault/create_db.py
 python manage.py migrate
 
 # create user, group, user_group, project, group_project
+echo "from django.contrib.auth.models import User, Group; grp = Group.objects.create(); grp.name = 'admins'; grp.save(); usr = User.objects.create_superuser('admin', 'admin@admin', 'password'); usr.groups.add(grp); usr.save()" | python manage.py shell
 
 # Run Vault server
 python manage.py runserver 0.0.0.0:8000
