fc780724e6bd8ae3344624620d3f515d21d28e14
Julio Rama <julio.rama@corp.globo.com>
2020-01-28 12:54:42 -0300

    Fix start.sh script in vault app

--- helpers/docker/vault/start.sh
+++ helpers/docker/vault/start.sh
@@ -6,7 +6,7 @@ python helpers/docker/vault/create_db.py
 python manage.py migrate
 
 # create user, group, user_group
-echo "execfile('helpers/docker/vault/populate_db.py')" | python manage.py shell
+echo "exec(open('helpers/docker/vault/populate_db.py').read())" | python manage.py shell
 
 # Run Vault server
 python manage.py runserver 0.0.0.0:8000
