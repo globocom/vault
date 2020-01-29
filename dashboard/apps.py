b1f934d807209cd34b9cd14fd85fb6126e9a8ad6
Julio Rama <julio.rama@corp.globo.com>
2020-01-29 17:01:28 -0300

    Removed old, unused widget-related code

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -7,4 +7,3 @@ from django.utils.translation import ugettext_lazy as _
 class DashboardConfig(AppConfig):
     name = 'dashboard'
     verbose_name = _("Dashboard")
-    dashboard_widgets = [{'widget_class': 'dashboard.widgets.NoTeamWarning'}]
--- dashboard/templatetags/dashboard_tags.py
+++ dashboard/templatetags/dashboard_tags.py
@@ -4,17 +4,8 @@ from django import template
 from django.apps import apps
 from django.conf import settings
 
-from dashboard.widgets import RenderWidgets
-
 
 register = template.Library()
-
-
-@register.tag
-def render_widgets(parser, token):
-    """Templatetag to render dashboard widgets"""
-
-    return RenderWidgets()
 
 
 @register.simple_tag
