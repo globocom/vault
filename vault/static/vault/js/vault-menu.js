a7ecb48a6110d772f92f6341c1fe2f721d4a94cd
Alan <avanselmo@gmail.com>
2021-09-09 01:58:53 -0300

    Menu fix

--- vault/static/vault/js/vault-menu.js
+++ vault/static/vault/js/vault-menu.js
@@ -12,22 +12,29 @@ const VaultMenu = (ENDPOINTS = []) => {
     components: {
       "sub-menu-item": SubMenuItem,
     },
-    props: ["item", "updateItem"],
+    props: ["item"],
+    computed: {
+      hasSub() {
+        return this.item.subitems?.length > 0;
+      },
+    },
     methods: {
-      toggleSubMenu() {
+      toggleSubMenu(event) {
+        if (this.hasSub) {
+          event.preventDefault();
+        }
         this.item.opened = !this.item.opened;
-        this.updateItem(this.item);
       },
     },
     template: `
     <li :class="{'submenu-active': item.opened}">
-      <a class="app-main-menu-item dropdown-toggle"
-         @click.prevent="toggleSubMenu"
+      <a class="app-main-menu-item" :class="{ 'dropdown-toggle': hasSub }"
+         @click="toggleSubMenu"
          :href="item.url"
          :title="item.name">
         <i :class="['icon', item.icon]"></i> {{ item.name }}
       </a>
-      <ul v-if="item.subitems?.length" class="sub-menu">
+      <ul v-if="hasSub" class="sub-menu">
         <sub-menu-item v-for="(subitem, index) in item.subitems"
                        :subitem="subitem"
                        :key="'s'+index">
@@ -45,39 +52,37 @@ const VaultMenu = (ENDPOINTS = []) => {
     data: {
       endpoints: ENDPOINTS,
       items: [],
+      updated: [],
     },
     async created() {
-      const currentItems = localStorage.getItem("vaultMenuApps");
+      const cached = localStorage.getItem("vaultMenuApps");
+      const updated = [];
 
-      if (currentItems) {
-        this.items = JSON.parse(currentItems);
+      if (cached) {
+        this.items = JSON.parse(cached);
       }
-
-      localStorage.setItem("vaultMenuApps", JSON.stringify([]));
 
       for (const url of this.endpoints) {
         const item = await this.fetchInfo(url);
         item.opened = false;
-        this.updateItem(item);
+        updated.push(item);
+        if (!cached) {
+          this.items.push(item);
+        }
       }
+
+      localStorage.setItem("vaultMenuApps", JSON.stringify(updated));
     },
     methods: {
       async fetchInfo(url) {
         const res = await fetch(`${url}?opt=menu`);
         return res.json();
       },
-      updateItem(item) {
-        if (!this.items.filter((i) => i.name == item.name).length) {
-          this.items.push(item);
-        }
-        localStorage.setItem("vaultMenuApps", JSON.stringify(this.items));
-      },
     },
     template: `
       <ul class="app-menus">
         <menu-item v-for="(item, index) in items"
                    :item="item"
-                   :updateItem="updateItem"
                    :key="'i'+index">
         </menu-item>
       </ul>
